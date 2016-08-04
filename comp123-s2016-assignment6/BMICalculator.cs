d26aa66de9cc1efb8ca411249c2095658043f3a4
jimmymcperson <undeadwalrus@hotmail.com>
2016-08-03 20:42:58 -0400

    Initial Commit

---
+++ comp123-s2016-assignment6/BMICalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comp123_s2016_assignment6
+{
+    /** <summary>
+    * This class defines a form for a BMI calculator.
+    * </summary>
+    * 
+    * @class BMICalculator
+    * @extends Form
+    */
+    public partial class BMICalculator : Form
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ comp123-s2016-assignment6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * ==================================================================================================
+ * Author: Jonathan Hao
+ * Student#: 300869273
+ * Date Modified: August 1, 2016
+ * Description: This is a demonstration of Windows Forms applications.
+ * Version: 0.0.1 - Initial Commit
+ * ==================================================================================================
+ */
+namespace comp123_s2016_assignment6
+{
+    /** <summary>
+    * This is the driver class for the program.
+    * </summary>
+    *
+    * @class Program
+    */
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        /** <summary>
+        * This is the "main" method.
+        * </summary>
+        * 
+        * @method Main
+        * @returns {void}
+        */
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BMICalculator());
+        }
+    }
+}
